d1d2057d5bb5a3bd27c37f70bd27a71ac6925b57
Keaton Burleson <keaton.burleson@me.com>
2020-02-26 15:29:48 -0600

    Adds assignment 4

---
+++ assignment4/src/main/java/Main.java
@@ -0,0 +1,30 @@
+import org.parboiled.Parboiled;
+import org.parboiled.common.StringUtils;
+import org.parboiled.errors.ErrorUtils;
+import org.parboiled.parserunners.ReportingParseRunner;
+import org.parboiled.support.ParsingResult;
+
+import static org.parboiled.support.ParseTreeUtils.printNodeTree;
+
+import java.util.Scanner;
+
+@SuppressWarnings("rawtypes")
+public class Main {
+    public static void main(String[] args) {
+        NameParser nameParser = Parboiled.createParser(NameParser.class);
+
+        while (true) {
+            System.out.print("Please input your name (single RETURN to exit): ");
+
+            String input = new Scanner(System.in).nextLine();
+            if (StringUtils.isEmpty(input)) break;
+
+            ParsingResult<?> result = new ReportingParseRunner(nameParser.FullName()).run(input);
+
+            if (!result.parseErrors.isEmpty())
+                System.out.println(ErrorUtils.printParseError(result.parseErrors.get(0)));
+            else
+                System.out.println(printNodeTree(result) + '\n');
+        }
+    }
+}
---
+++ assignment4/src/main/java/NameParser.java
@@ -0,0 +1,27 @@
+import org.parboiled.BaseParser;
+import org.parboiled.Rule;
+import org.parboiled.annotations.BuildParseTree;
+
+@BuildParseTree
+public class NameParser extends BaseParser<Object> {
+    Rule FullName() {
+        return Sequence(
+                Name(),
+                Space(),
+                Name()
+        );
+    }
+
+    Rule Space() {
+        return Ch(' ');
+    }
+
+    Rule Name() {
+        return OneOrMore(Letter());
+    }
+
+    Rule Letter() {
+        return FirstOf(CharRange('a', 'z'), CharRange('A', 'Z'));
+    }
+
+}
