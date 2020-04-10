6dd7f872631fd53476cec49925461291d0569ce5
Keaton Burleson <keaton.burleson@me.com>
2020-04-09 19:50:00 -0500

    Finishes quiz 9

---
+++ quiz9/Question_2/Question_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Question_2
+{
+    public delegate void ExampleDelegate();
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExampleDelegate exampleDelegate = new ExampleDelegate(SayHello);
+
+            exampleDelegate();
+        }
+
+        static void SayHello()
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+}
